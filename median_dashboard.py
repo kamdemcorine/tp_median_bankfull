774f380c4b4fe8d41a5dbd947eaf14adb5a34677
kamdemcorine <127659943+kamdemcorine@users.noreply.github.com>
2024-12-17 11:50:42 +0100

    Update median_dashboard.py

--- median_dashboard.py
+++ median_dashboard.py
@@ -1,1 +1,8 @@
+import pandas as pd
+import numpy as np
+import altair as alt
 
+data = pd.read_csv('bank-full.csv')
+
+chart = alt.Chart(data).mark_point().encode( x='age', y='age' ) 
+chart.show()
